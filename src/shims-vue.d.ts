a5f907e4eb7bd5f6304dd98ef9b40d498aca5d6f
crazyzpl <crazyzplzpl@163.com>
2021-12-22 18:51:12 +0800

    add value to declare module

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,11 +1,20 @@
 // Mocks all files ending in `.vue` showing them as plain Vue instances
 /* eslint-disable */
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
 
-declare module "*.svg"
-declare module "*.png"
-declare module "*.jpg"
+declare module '*.svg' {
+  const value: string
+  export default value
+}
+declare module '*.png' {
+  const value: string
+  export default value
+}
+declare module '*.jpg' {
+  const value: string
+  export default value
+}
